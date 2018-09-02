631fd54140706ab2a880f3831ac98ee1c2fefa95
Joe Cross <joe.mcross@gmail.com>
2018-09-01 22:10:10 -0700

    move Sentinel to misc

--- scaffolding/__init__.py
+++ scaffolding/__init__.py
@@ -1,33 +1,3 @@
-import functools
+from .misc import Sentinel
+
 __all__ = ["Sentinel"]
-
-
-def singleton(cls):
-    instances = {}
-
-    @functools.wraps(cls)
-    def get(name: str):
-        try:
-            return instances[name]
-        except KeyError:
-            o = instances[name] = cls(name)
-            return o
-    return get
-
-
-@singleton
-class Sentinel:
-    """Singleton object with a templated representation"""
-    template = "<{}>"
-
-    def __init__(self, name):
-        """
-        >>> Sentinel('a') is Sentinel('a')
-        True
-        >>> Sentinel('a') is Sentinel('b')
-        False
-        """
-        self.name = name
-
-    def __repr__(self):
-        return Sentinel.template.format(self.name, name=self.name)
