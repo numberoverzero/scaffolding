958d18bcacbbb6e3cfb3c790ce510c7527777105
Joe Cross <joe.mcross@gmail.com>
2018-09-03 22:40:15 -0700

    Patch falcon.API to handle empty json body

--- scaffolding/__init__.py
+++ scaffolding/__init__.py
@@ -1,4 +1,37 @@
+"""
+When constructing a new API, use either of the following forms::
+
+    >>> import scaffolding
+    >>> api = scaffolding.API(
+    ...     middleware=[...]
+    ... )
+    >>> api = falcon.API(
+    ...     request_type=scaffolding.Request,
+    ...     middleware=[...]
+    ... )
+
+otherwise using the default falcon.Request class,
+reading an empty-but-valid json body twice raises:
+
+    https://github.com/falconry/falcon/issues/1234
+    https://github.com/falconry/falcon/pull/1311
+"""
+import falcon
+import functools
 from .misc import Missing, Sentinel
+from .patches import Request
+
+__all__ = ["Sentinel", "Missing", "Request", "API"]
 
 
-__all__ = ["Sentinel", "Missing"]
+class API(falcon.API):
+    @functools.wraps(falcon.API.__init__)
+    def __init__(
+            self,
+            media_type=falcon.DEFAULT_MEDIA_TYPE,
+            request_type=Request,
+            response_type=falcon.Response,
+            middleware=None,
+            router=None,
+            independent_middleware=False):
+        super().__init__(media_type, request_type, response_type, middleware, router, independent_middleware)
---
+++ scaffolding/patches.py
@@ -0,0 +1,25 @@
+import falcon
+
+__all__ = ["Request"]
+
+
+class Request(falcon.Request):
+    @property
+    def media(self):
+        # XXX @numberoverzero
+        # XXX remove when https://github.com/falconry/falcon/issues/1234 is released to pypi
+        # XXX otherwise an empty body "{}" will raise on second access
+        if self._media is not None:
+            return self._media
+
+        handler = self.options.media_handlers.find_by_media_type(
+            self.content_type,
+            self.options.default_media_type
+        )
+
+        # Consume the stream
+        raw = self.bounded_stream.read()
+
+        # Deserialize and Return
+        self._media = handler.deserialize(raw)
+        return self._media
