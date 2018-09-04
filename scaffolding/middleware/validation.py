d0db5d1ae12b3287934ece015d7cce25cbfc86fa
Joe Cross <joe.mcross@gmail.com>
2018-09-03 20:05:46 -0700

    simplify OpenApiRequestValidation, add body validation

--- scaffolding/middleware/validation.py
+++ scaffolding/middleware/validation.py
@@ -5,20 +5,6 @@ from ..exc import Exceptions
 from ..openapi import Operation, Specification
 
 
-error_messages = {
-    "null": "missing",
-    "required": "missing",
-    "type": "type",
-    "validator_failed": "type"
-}
-field_classes = {
-    "boolean": ma.fields.Boolean,
-    "integer": ma.fields.Integer,
-    "number": ma.fields.Number,
-    "string": ma.fields.String,
-}
-
-
 class OpenApiRequestValidation:
     def __init__(self, spec: Specification) -> None:
         self.spec = spec
@@ -26,7 +12,8 @@ class OpenApiRequestValidation:
     def process_resource(self, req: falcon.Request, resp: falcon.Response, resource, params: dict) -> None:
         operation = self.spec.operations.by_req(req)
         OpenApiRequestValidation.collect_params(operation, req, params)
-        OpenApiRequestValidation.validate_params(operation, params)
+        operation.validate_params(params)
+        operation.validate_body(req.media)
 
     @staticmethod
     def collect_params(operation: Operation, req: falcon.Request, params: dict) -> None:
@@ -47,19 +34,3 @@ class OpenApiRequestValidation:
                 raise Exceptions.internal_error()
             if f.required and v is ma.missing:
                 raise Exceptions.missing_parameter(name)
-
-    @staticmethod
-    def validate_params(operation: Operation, params: dict) -> None:
-        schema = operation.param_schema
-        data, errors = schema.load(params)
-        if errors:
-            # TODO for now just send back the first error
-            name, error = next(iter(errors.items()))
-            if error == "type":
-                raise Exceptions.invalid_parameter(name, params[name], type=schema.fields[name].type_name)
-            elif error == "missing":
-                raise Exceptions.missing_parameter(name)
-            else:
-                raise Exceptions.internal_error()
-        params.clear()
-        params.update(data)
