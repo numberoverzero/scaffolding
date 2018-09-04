07d893ec8f881b02aa7be73167f701f9e9a07178
Joe Cross <joe.mcross@gmail.com>
2018-09-03 20:38:44 -0700

    fix _StructuredError.handle setting incorrect http_status

--- scaffolding/exc.py
+++ scaffolding/exc.py
@@ -10,14 +10,15 @@ logger = logging.getLogger(__name__)
 
 
 class _ErrorCode(enum.Enum):
-    NotFound = (404, "NotFound")
-    NotAuthenticated = (401, "NotAuthenticated")
-    InvalidParameter = (400, "InvalidParameter")
-    MissingParameter = (400, "MissingParameter")
-    InternalError = (500, "InternalError")
+    NotFound = (404, falcon.status.HTTP_404, "NotFound")
+    NotAuthenticated = (401, falcon.status.HTTP_401, "NotAuthenticated")
+    InvalidParameter = (400, falcon.status.HTTP_400, "InvalidParameter")
+    MissingParameter = (400, falcon.status.HTTP_400, "MissingParameter")
+    InternalError = (500, falcon.status.HTTP_500, "InternalError")
 
-    def __init__(self, http_status: int, id: str) -> None:
-        self.http_status = http_status
+    def __init__(self, http_status_code: int, http_status_line: str, id: str) -> None:
+        self.http_status_code = http_status_code
+        self.http_status_line = http_status_line
         self.id = id
 
     def new(self, message: str) -> "_StructuredError":
@@ -31,9 +32,9 @@ class _StructuredError(Exception):
 
     @staticmethod
     def handle(ex: "_StructuredError", req: falcon.Request, resp: falcon.Response, *_, **__) -> None:
-        log = logger.error if ex.code.http_status == 500 else logger.info
-        log(f"{ex.code.id} during {req.method} {req.path}")
-        resp.status = ex.code.http_status
+        log = logger.error if ex.code.http_status_code == 500 else logger.info
+        log(f"{ex.code.http_status_code} {ex.code.id} during {req.method} {req.path}")
+        resp.status = ex.code.http_status_line
         resp.media = {
             "code": ex.code.id,
             "message": ex.message
